066dbace024c622bbbb1ffc1592239cd453fbbf9
natbonn <natbon@hotmail.gr>
2026-09-03 12:05:24 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/cf/starter_mavenpro/StarterMavenproApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starter_mavenpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterMavenproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterMavenproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/starter_mavenpro/StarterMavenproApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starter_mavenpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterMavenproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
