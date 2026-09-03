55d2d7e68e6b11dd6b8b59f015d6ecc22945e112
natbonn <natbon@hotmail.gr>
2026-09-03 19:12:09 +0300

    Add welcome.html template for greeting display

--- src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
+++ src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/starter-maven")
@@ -19,5 +20,12 @@ public class HelloController {
     public String hello2(Model model) {
         model.addAttribute("message", "Hello Coding!!!");
         return "index";
+    }
+
+    @GetMapping("/v1/welcome")
+    public String welcome(@RequestParam(name = "username", defaultValue = "Guest") String username,
+                          Model model) {
+        model.addAttribute("name", username);
+        return "welcome";
     }
 }
